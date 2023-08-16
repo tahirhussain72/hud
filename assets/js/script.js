9c7ae3909e4d8fba3b3d7e190eb22a32f3005fbd
Tahir <tahir.toorie72@gmail.com>
2023-08-17 01:17:55 +0500

    2nd commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,18 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+  slidesPerView: 4,
+  spaceBetween: 20,
+  keyboard: {
+    enabled: true,
+  },
+  // grid: {
+  //   column: 3,
+  // },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
