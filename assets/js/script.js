a51edde750e4bb646d2a8c674047d68175100eb1
Tahir <tahir.toorie72@gmail.com>
2023-08-17 17:45:40 +0500

    3rd commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,45 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+
+
+
+// (function(){
+//   var dd = $('dd');
+//   dd.filter(':nth-child(n+3)').hide();
+//   $('dl').on('click', 'dt', function(){
+//     $(this)
+//       .next()
+//       .slideDown(200)
+//       .siblings('dd')
+//       .slideUp(200);
+//   })
+// })();
+
+
+
+$(document).ready(function() {
+  function close_accordion_section() {
+      $('.accordion .accordion-section-title').removeClass('active');
+      $('.accordion .accordion-section-content').slideUp(300).removeClass('open');
+  }
+
+  $('.accordion-section-title').click(function(e) {
+      // Grab current anchor value
+      var currentAttrValue = $(this).attr('href');
+
+      if($(e.target).is('.active')) {
+          close_accordion_section();
+      }else {
+          close_accordion_section();
+
+          // Add active class to section title
+          $(this).addClass('active');
+          // Open up the hidden content panel
+          $('.accordion ' + currentAttrValue).slideDown(300).addClass('open');
+      }
+
+      e.preventDefault();
+  });
+});
